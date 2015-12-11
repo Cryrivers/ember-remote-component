d7caf1c4353f5f971a69cb60b8103a65877a2ada
LABS-WANGZ-MAC <wangz@garena.com>
2015-12-11 12:08:59 +0800

    Bug Fixes

--- blueprints/remote-component/files/app/remote-components/__name__/component.js
+++ blueprints/remote-component/files/app/remote-components/__name__/component.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 import RemoteComponentMixin from 'ember-remote-component/mixins/remote-component-mixin';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(RemoteComponentMixin, {
 });
