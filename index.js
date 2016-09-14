a9561f71675cda0af2a2e3c512fa29f5da7941ad
Wang Zhongliang <wangz@garena.com>
2016-09-14 16:31:32 +0800

    minor cleanup

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 'use strict';
 var Funnel = require('broccoli-funnel');
 var Filter = require('broccoli-filter');
-var Concat = require('broccoli-concat');
-var MergeTrees = require('broccoli-merge-trees');
 
 var remoteComponentTree = null;
 
 function RemoteComponentFilter(inputNode) {
   if (!(this instanceof RemoteComponentFilter)) {
-    return new RemoteComponentFilter(inputNode)
+    return new RemoteComponentFilter(inputNode);
   }
   Filter.call(this, inputNode, {
     annotation: 'Remote Component Compilation'
@@ -19,7 +17,7 @@ RemoteComponentFilter.prototype = Object.create(Filter.prototype);
 RemoteComponentFilter.prototype.constructor = RemoteComponentFilter;
 RemoteComponentFilter.prototype.extensions = ['js'];
 RemoteComponentFilter.prototype.targetExtension = 'js';
-RemoteComponentFilter.prototype.processString = function(content, relativePath) {
+RemoteComponentFilter.prototype.processString = function(content) {
   var updateContent = content.replace(/'(.+)\/remote-components\/(.+)\/component'/g, '\'$1/components/$2\'');
   updateContent = updateContent.replace(/"(.+)\/remote-components\/(.+)\/template"/g, '"$1/templates/components/$2"');
   updateContent = updateContent.replace(/"(.+)\/remote-components\/(.+)\/template.hbs"/g, '"$1/templates/components/$2.hbs"');
@@ -28,9 +26,6 @@ RemoteComponentFilter.prototype.processString = function(content, relativePath) 
 
 module.exports = {
   name: 'ember-remote-component',
-  isDevelopingAddon: function() {
-    return true;
-  },
   postprocessTree(type, tree) {
     if (type === 'js') {
       var normalAppTree = new Funnel(tree, {
