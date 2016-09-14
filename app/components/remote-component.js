5d2fa6875ef95f58abd4f9bea0005a415d633435
Wang Zhongliang <wangz@garena.com>
2016-09-14 15:59:24 +0800

    Fix duplicate component resolving

--- app/components/remote-component.js
+++ app/components/remote-component.js
@@ -6,22 +6,27 @@ import getOwner from 'ember-getowner-polyfill';
 
 const RemoteComponent = Ember.Component.extend({
   tagName: '',
+  isFailed: false,
+  isLoading: false,
   isLoaded: false,
   didReceiveAttrs() {
     const container = getOwner(this);
     const componentName = this.get('componentName');
     const componentResolved = container.hasRegistration(`component:${ componentName }`);
+    const isLoading = this.get('isLoading');
     if (componentResolved) {
       this.set('isLoaded', true);
-    } else {
+    } else if (!isLoading) {
+      this.set('isLoading', true);
       $.when(
-          $.getScript(`/remote-components/${ componentName }/component.js`),
-          $.getScript(`/remote-components/${ componentName }/template.js`)
+        $.getScript(`/remote-components/${ componentName }/component.js`),
+        $.getScript(`/remote-components/${ componentName }/template.js`)
       ).done(()=> {
         let container = getOwner(this);
         container.register(`component:${ componentName }`, require(`${ ENV.modulePrefix }/components/${ componentName }`).default, {singleton: false});
         this.set('isLoaded', true);
       }).fail(()=> {
+        this.set('isFailed', true);
       });
     }
   }
